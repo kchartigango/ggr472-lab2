1d5c747e95a5688adf23bd3c75d7e7a70f14f40f
kchartigango <kchgo7@gmail.com>
2024-02-20 15:40:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,42 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoia2NoZ28iLCJhIjoiY2xzYzl0ZmdqMGV2MDJrc2J0d2QxY
 
 const map = new mapboxgl.Map({
     container: 'my-map',
-    style: 'mapbox://styles/mapbox/outdoors-v12',
+    style: 'mapbox://styles/kchgo/clsupzcy5003k01p4749laxdc',
     center: [-79.2, 43.8],
     zoom: 9,
 });
 
 map.on('load', () => {
 
-    //Adding a data source for the GeoJSON file.
-    map.addSource('restaurants-data', {
+    //Adding a data source for the Toronto parks GeoJSON file.
+    map.addSource('parks-data', {
         type: 'geojson',
-        data: 'https://raw.githubusercontent.com/kchartigango/ggr472-lab2/main/restaurants.geojson'
+        data: 'https://raw.githubusercontent.com/kchartigango/ggr472-lab2/main/parks.geojson'
+    });
+
+    map.addLayer({
+        'id': 'parks-point',
+        'type': 'circle',
+        'source': 'parks-data',
+        'paint': {
+            'circle-radius': 12,
+            'circle-color': '#00a86b',
+            'circle-stroke-color': 'black',
+            'circle-stroke-width': 1 
+        }
+    });
+
+    //Adding a data source from a Mapbox tileset
+    map.addSource('restaurants-point', {
+        'type': 'geojson',
+        'url': 'mapbox://kchgo.ar3w32dg'
     });
 
     map.addLayer({
         'id': 'restaurants-point',
         'type': 'circle',
-        'source': 'restaurants-data',
-        'paint': {
-            'circle-radius': 8,
-            'circle-color': '#b175e8'
-        }
+        'source': 'restaurants-point',
+        'source-layer': 'restaurants-8crsso'
     });
+
 });
