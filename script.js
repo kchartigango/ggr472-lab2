11d48c4273956ee20a31e567308c3eb30aaaef47
kchartigango <kchgo7@gmail.com>
2024-02-20 14:31:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoia2NoZ28iLCJhIjoiY2xzYzl0ZmdqMGV2MDJrc2J0d2QxY
 
 const map = new mapboxgl.Map({
     container: 'my-map',
-    style: 'mapbox://styles/mapbox/streets-v12',
+    style: 'mapbox://styles/kchgo/clsupzcy5003k01p4749laxdc',
     center: [-79.2, 43.8],
     zoom: 9,
 });
@@ -12,6 +12,16 @@ map.on('load', () => {
     //Adding a data source for the GeoJSON file.
     map.addSource('restaurants-data', {
         type: 'geojson',
-        data: ''
-    })
+        data: 'https://raw.githubusercontent.com/kchartigango/ggr472-lab2/main/restaurants.geojson'
+    });
+
+    map.addLayer({
+        'id': 'restaurants-point',
+        'type': 'circle',
+        'source': 'restaurants-data',
+        'paint': {
+            'circle-radius': 7,
+            'circle-color': '#b175e8'
+        }
+    });
 })
