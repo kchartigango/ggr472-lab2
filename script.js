89d849f62e253e23c31425af7e2d242c3f1679cf
kchartigango <kchgo7@gmail.com>
2024-02-20 16:43:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,51 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoia2NoZ28iLCJhIjoiY2xzYzl0ZmdqMGV2MDJrc2J0d2QxY
 const map = new mapboxgl.Map({
     container: 'my-map',
     style: 'mapbox://styles/kchgo/clsupzcy5003k01p4749laxdc',
-    center: [-79.2, 43.8],
-    zoom: 9,
+    center: [-79.35, 43.7],
+    zoom: 10,
 });
 
 map.on('load', () => {
 
+    //Adding a data source containing GeoJSON data and visualizing it as a layer.
+    map.addSource('sport-data', {
+        type: 'geojson',
+        data: {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+                    'properties': {
+                        'name': 'SportChek - Toronto'
+                    },
+                    'geometry': {
+                        'coordinates': [
+                            -79.38037044907225,
+                            43.6428149999997
+                        ],
+                        'type': 'Point'
+                    }
+                }
+            ]
+        }
+    });
+
+    map.addLayer({
+        'id': 'sport-point',
+        'type': 'circle',
+        'source': 'sport-data',
+        'paint': {
+            'circle-radius':12,
+            'circle-color': '#ac86a8',
+            'circle-stroke-color': 'black',
+            'circle-stroke-width': 1
+        }
+    });
+
     //Adding a data source for the Toronto parks GeoJSON file.
     map.addSource('parks-data', {
         type: 'geojson',
-        data: 'https://raw.githubusercontent.com/kchartigango/ggr472-lab2/main/parks.geojson'
+        data: 'https://raw.githubusercontent.com/kchartigango/ggr472-lab2/main/parks-updated.geojson'
     });
 
     map.addLayer({
@@ -20,24 +55,11 @@ map.on('load', () => {
         'type': 'circle',
         'source': 'parks-data',
         'paint': {
-            'circle-radius': 12,
+            'circle-radius': 10,
             'circle-color': '#00a86b',
             'circle-stroke-color': 'black',
             'circle-stroke-width': 1 
         }
-    });
-
-    //Adding a data source from a Mapbox tileset
-    map.addSource('restaurants-point', {
-        'type': 'geojson',
-        'url': 'mapbox://kchgo.ar3w32dg'
-    });
-
-    map.addLayer({
-        'id': 'restaurants-point',
-        'type': 'circle',
-        'source': 'restaurants-point',
-        'source-layer': 'restaurants-8crsso'
     });
 
 });
